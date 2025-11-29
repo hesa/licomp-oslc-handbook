4a8ee71f9813f3d0f207df21a16e14c3473ad68e
hesa <hesa@localhost>
2025-11-29 22:27:01 +0000

    Release 0.1.2

--- licomp_oslc_handbook/config.py
+++ licomp_oslc_handbook/config.py
@@ -2,7 +2,7 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-licomp_oslc_handbook_version = '0.1.1'
+licomp_oslc_handbook_version = '0.1.2'
 my_supported_api_version = '0.5'
 
 cli_name = 'licomp-oslc_handbook'
